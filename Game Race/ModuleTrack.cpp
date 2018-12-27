bd44096ba2b0a4097b33674dbb3ded17b736401d
Nadine044 <36292059+Nadine044@users.noreply.github.com>
2018-12-27 21:06:09 +0100

    ModuleTrack added

---
+++ Game Race/ModuleTrack.cpp
@@ -0,0 +1,47 @@
+#include "Globals.h"
+#include "Application.h"
+#include "PhysBody3D.h"
+#include "Primitive.h"
+#include "ModuleTrack.h"
+
+ModuleTrack::ModuleTrack(Application* app, bool start_enable) : Module(app, start_enable) {
+
+	wall_mass = 0.0f;
+}
+
+ModuleTrack::~ModuleTrack() {}
+
+//Load assets
+
+bool ModuleTrack::Start() {
+
+	LOG("Loading track circuit");
+
+	//Wall mass
+	wall_mass = 30.0f;
+
+	//Track walls
+	wall.size.x = 10;
+	wall.size.y = 10;
+	wall.size.z = 10;
+
+	wall.SetPos(0, 50, 0);
+	App->physics->AddBody(wall, wall_mass);
+
+	return true;
+}
+
+bool ModuleTrack::CleanUp() {
+
+	LOG("Unloading track circuit");
+
+	return true;
+}
+
+
+update_status ModuleTrack::Update(float dt) {
+
+	wall.Render();
+
+	return UPDATE_CONTINUE;
+}
---
+++ Game Race/ModuleTrack.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Module.h"
+#include "Globals.h"
+#include "p2Point.h"
+#include "Primitive.h"
+
+//EN START() CREATE BODY, HACER FUNCIÓN PARA RECOPILAR TODAS LAS WALL
+
+class ModuleTrack : public Module {
+public:
+	ModuleTrack(Application* app, bool start_enable = true);
+	virtual ~ModuleTrack();
+
+	bool Start();
+	update_status Update(float dt);
+	bool CleanUp();
+
+public:
+	Cube wall;
+	float wall_mass;
+
+};
